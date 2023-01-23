18bfbf0266cc63354e337264cc44c7c6e0018921
Nayan Singhal <96867955+nayansinghal23@users.noreply.github.com>
2023-01-23 09:39:13 +0530

    Find the town judge

---
+++ 23.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-the-town-judge/
+
+/*
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>> trust) {
+        if (trust.size() == 0 && n == 1) 
+            return 1;
+        vector<int> count(n + 1);
+        for (auto person : trust) {
+            count[person[0]]--;
+            count[person[1]]++;
+        }
+        for (int person = 0; person < count.size(); person++) {
+            if (count[person] == n - 1) return person;
+        }
+        return -1;
+    }
+};
+*/
